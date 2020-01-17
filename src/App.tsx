9a4aba4b90c1f3856b375e9440799b71c4d8c8e5
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2020-01-17 16:34:41 +0000

    Display rendered Wordpress content in React using dangerouslySetInnerHTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,39 @@
-import  React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 const App: React.FC = () => {
-
   const [posts, updatePosts] = useState([]);
 
   fetch(`http://test.local/wp-json/wp/v2/posts`)
-  .then((response)=> response.json())
-  .then((json)=> {
-    updatePosts(json);
-  })
+    .then(response => response.json())
+    .then(json => {
+      updatePosts(json);
+    });
 
   return (
     <div className="App">
-      {posts && posts.map((post: any)=> {
-        // return <pre>{JSON.stringify(posts, null, "  ")}</pre>
-              return <>
-                <h2>{(post.title.rendered.toString())}</h2>
+      {posts &&
+        posts.map((post: any) => {
+          return (
+            <>
+              <h2 dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
 
-                <p>{(post.content.rendered.toString())}</p>
-              
-              </>
-
-      })}
+              <div
+                dangerouslySetInnerHTML={{ __html: post.content.rendered }}
+              />
+            </>
+          );
+        })}
 
       <hr />
 
-      <h2><pre>Debug section</pre></h2>
+      <h2>
+        <pre>Debug section</pre>
+      </h2>
 
       <pre>{JSON.stringify(posts, null, "  ")}</pre>
     </div>
   );
-}
+};
 
 export default App;
